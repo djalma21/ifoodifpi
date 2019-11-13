74398fc6966fcbbd7bbddceb1e9f01f7e88c351e
djalma21 <djalma21cruz@gmail.com>
2019-11-13 09:16:37 -0300

    autenticação permissão

--- ifoodifpi/settings.py
+++ ifoodifpi/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'corsheaders',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ifoodifpi.urls'

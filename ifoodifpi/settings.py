ba1c1d4dd05e282ab765386faa86a86fed110b5d
djalma21 <djalma21cruz@gmail.com>
2019-11-14 12:20:59 -0300

    autenticação permissão erro

--- ifoodifpi/settings.py
+++ ifoodifpi/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'ifoodifpi.urls'
